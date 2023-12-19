b3a0bf12d60b3ce2c9b153ef9a349dfbab113935
Robin Naghshbandi <57363833+robbandi@users.noreply.github.com>
2023-12-19 01:08:56 +0000

    Initial Commit

---
+++ TotalSize/TotalSizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalSizeApp.swift
+//  TotalSize
+//
+//  Created by Persona on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TotalSizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
